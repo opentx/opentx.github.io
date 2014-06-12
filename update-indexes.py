cb2c21901c4258927a08c1242fcf81e38e29d236
Kjell Kernen <kjell.kernen@gmail.com>
2014-06-12 10:10:28 +0200

    Indexes automatically sorted alphabetically

--- update-indexes.py
+++ update-indexes.py
@@ -1,20 +1,30 @@
 import os
+
+print 'Updating icons-taranis' 
 f = open('_data/icons-taranis.yml', 'w')
-for file in os.listdir("assets/images/icons-taranis"):
+l = os.listdir("assets/images/icons-taranis")
+l.sort()
+for file in l:
   s = '- name: "{}"'.format(file)
   print s
   f.write(s) 
   f.write('\n') 
 
+print 'Updating screens-taranis' 
 f = open('_data/screens-taranis.yml', 'w')
-for file in os.listdir("assets/images/screens-taranis"):
+l = os.listdir("assets/images/screens-taranis")
+l.sort()
+for file in l:
   s = '- name: "{}"'.format(file)
   print s
   f.write(s) 
   f.write('\n') 
 
+print 'Updating screens-9x' 
 f = open('_data/screens-9x.yml', 'w')
-for file in os.listdir("assets/images/screens-9x"):
+l = os.listdir("assets/images/screens-9x")
+l.sort()
+for file in l:
   s = '- name: "{}"'.format(file)
   print s
   f.write(s) 
